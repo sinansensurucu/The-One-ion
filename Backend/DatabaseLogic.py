51bcd1ed1fee960fac053b627eefa844f5348568
sinansensurucuwewalk <sinan@wewalk.io>
2025-03-18 21:19:01 +0000

    wrapped up database logic

--- Backend/DatabaseLogic.py
+++ Backend/DatabaseLogic.py
@@ -2,10 +2,11 @@ from flask import session
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 import requests
+import random
 import os
 
 firebaseCredentials = credentials.Certificate(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Credentials', 'firebaseCredentials.json'))
-APIKey = ''
+APIKey = 'AIzaSyCZmBaKVQJfx-ZF16BByk7Unt8K2LzDjK4'
 
 firebase_admin.initialize_app(firebaseCredentials)
 db = firestore.client()
@@ -13,14 +14,9 @@ db = firestore.client()
 class ExecutionAbort(Exception):
     pass
 
-def verifyUserToken(userIDToken):
-    try:
-        decodedUserToken = auth.verify_id_token(userIDToken)
-        print("[AUTH] Successfully verified user token.")
-        return decodedUserToken["uid"]
-    except Exception as e:
-        raise ExecutionAbort("[AUTH] Invalid token or verification failed.") from e
+#Authentication Functionality
 
+#returns user_id as string to frontend after signing in user
 def signInUser(email, password):
     signInURL = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={APIKey}"
     payload = {
@@ -34,11 +30,12 @@ def signInUser(email, password):
     
     if "idToken" in data:
         print("[AUTH] Login successful.")
-        return verifyUserToken(data["idToken"])
+        return __verifyUserToken__(data["idToken"])
     else:
         print("[AUTH] Login unsuccessful, credentials are incorrect.")
         raise ExecutionAbort("[AUTH] Login failed, credentials are incorrect.")
 
+#returns user_id as string to frontend after creating a user
 def createUser(email, password):
     try:
         auth.get_user_by_email(email)
@@ -47,67 +44,81 @@ def createUser(email, password):
         try:
             auth.create_user(email=email, password=password)
             user_id = signInUser(email, password)
-            createUserData(email, user_id)
+            __createUserData__(email, user_id)
             print("[AUTH] Created new user with email:", email)
             return user_id
-        except Exception as e:
-            raise ExecutionAbort("[AUTH] Failed to create user with provided email and password.") from e
+        except:
+            raise ExecutionAbort("[AUTH] Failed to create user with provided email and password.")
     except ValueError:
         raise ExecutionAbort("[AUTH] Either email or password has invalid format/characters, try again.")
 
+#deletes user account without returning anything
 def deleteUser(user_id):
     if not user_id:
         raise ExecutionAbort("[AUTH] User is not signed in.")
     
     try:
         auth.delete_user(user_id)
-        deleteUserData(user_id)
+        __deleteUserData__(user_id)
         print("[AUTH] Successfully deleted the user.")
-    except Exception as e:
-        raise ExecutionAbort("[AUTH] Failed to delete user.") from e
+    except:
+        raise ExecutionAbort("[AUTH] Failed to delete user.")
 
-def createUserData(email, user_id):
+#METHODS BELOW NOT FOR FRONTEND CALL
+def __verifyUserToken__(userIDToken):
+    try:
+        decodedUserToken = auth.verify_id_token(userIDToken)
+        print("[AUTH] Successfully verified user token.")
+        return decodedUserToken["uid"]
+    except:
+        raise ExecutionAbort("[AUTH] Invalid token or verification failed.")
+    
+def __createUserData__(email, user_id):
     if not user_id:
         raise ExecutionAbort("[DATA] Cannot create data for user that is not signed in.")
     
     userData = {
-        "userEmail": email,
-        "streak": 1,
-        "totalScore": 0,
-        "bestScore": 0,
-        "globalRanking": len(db.collection("users").get()) + 1
+        "userEmail" : email,
+        "streak" : 1,
+        "totalScore" : 0,
+        "bestScore" : 0,
+        "globalRanking" : len(db.collection("users").get()) + 1,
+        "readArticles" : []
     }
     db.collection("users").document(user_id).set(userData)
     print("[DATA] Successfully created user data fields.")
 
-def deleteUserData(user_id):
+def __deleteUserData__(user_id):
     if not user_id:
         raise ExecutionAbort("[DATA] Cannot delete data for user that is not signed in.")
     
     db.collection("users").document(user_id).delete()
     print("[DATA] Successfully deleted all user data.")
-    
 
-def registerScore(roundScore):
-    user_id = session.get("user_id")
 
+
+#User Profile Functionality
+
+#registers new score to user profile without returning anything
+def registerScore(user_id, score):
     if user_id is None:
         raise ExecutionAbort("[DATA] Cannot update score data for user that is not signed in.")
     
-    userSnapshot = db.collection("users").document(user_id).get().to_dict()
-    currentTotalScore = getUserTotalScore(user_id)
-    currentBestScore = getUserBestScore(user_id)
-    
-    db.collection("users").document(user_id).update({"totalScore": (currentTotalScore + roundScore)})
-    print("[DATA] Updated user's total score.")
-    
-    if roundScore > currentBestScore:
-        db.collection("users").document(user_id).update({"bestScore": roundScore})
-        print("[DATA] Updated user's best score.")
+    try:
+        currentTotalScore = getUserTotalScore(user_id)
+        currentBestScore = getUserBestScore(user_id)
+        
+        db.collection("users").document(user_id).update({"totalScore": (currentTotalScore + score)})
+        print("[DATA] Updated user's total score.")
+        
+        if score > currentBestScore:
+            db.collection("users").document(user_id).update({"bestScore": score})
+            print("[DATA] Updated user's best score.")
+    except:
+        raise ExecutionAbort("[DATA] Error while registering score to user.")
 
-def getUserTotalScore():
-    user_id = session.get("user_id")
-
+#returns integer value containing user's total score
+def getUserTotalScore(user_id):
     if user_id is None:
         raise ExecutionAbort("[DATA] Cannot get score data for user that is not signed in.")
 
@@ -115,12 +126,11 @@ def getUserTotalScore():
         userSnapshot = db.collection("users").document(user_id).get().to_dict()
         print("[DATA] Successfully fetched user's total score.")
         return userSnapshot.get("totalScore", 0)
-    except Exception as e:
+    except:
         raise ExecutionAbort("[DATA] Error while fetching user's total score.")
 
-def getUserBestScore():
-    user_id = session.get("user_id")
-
+#returns integer value containing user's best score
+def getUserBestScore(user_id):
     if user_id is None:
         raise ExecutionAbort("[DATA] Cannot get best score data for user that is not signed in.")
 
@@ -128,5 +138,121 @@ def getUserBestScore():
         userSnapshot = db.collection("users").document(user_id).get().to_dict()
         print("[DATA] Successfully fetched user's best score.")
         return userSnapshot.get("bestScore", 0)
-    except Exception as e:
+    except:
         raise ExecutionAbort("[DATA] Error while fetching user's best score.")
+    
+#sets users streak without returning anything
+def setUserStreak(user_id, streak):
+    if user_id is None:
+        raise ExecutionAbort("[DATA] Cannot update streak data for user that is not signed in.")
+    
+    try:
+        db.collection("users").document(user_id).update({"streak": streak})
+        print("[DATA] Updated user's streak.")
+    except:
+        raise ExecutionAbort("[DATA] Error while updating user's streak.")
+
+#returns integer value containing user's current streak
+def getUserStreak(user_id):
+    if user_id is None:
+        raise ExecutionAbort("[DATA] Cannot get streak data for user that is not signed in.")
+    
+    try:
+        userSnapshot = db.collection("users").document(user_id).get().to_dict()
+        print("[DATA] Successfully fetched user's streak.")
+        return userSnapshot.get("streak", 0)
+    except:
+        raise ExecutionAbort("[DATA] Error while fetching user's streak.")
+
+#returns integer value containing user's global ranking
+def getGlobalRanking(user_id):
+    if user_id is None:
+        raise ExecutionAbort("[DATA] Cannot get ranking data for user that is not signed in.")
+    
+    try:
+        userSnapshot = db.collection("users").get()
+
+        user_scores = []
+
+        for doc in userSnapshot:
+            data = doc.to_dict()
+            if data is None:
+                continue
+            score = data.get("totalScore", 0)
+            user_scores.append((doc.id, score))
+
+        user_scores = sorted(user_scores, key = lambda x: x[1], reverse = True)
+
+        for rank, (uid, score) in enumerate(user_scores, start = 1):
+            if uid == user_id:
+                return rank
+            
+        print("[DATA] Successfully fetched user's ranking.")
+        
+    except:
+        raise ExecutionAbort("[DATA] Error while fetching user's ranking.")
+
+#Article Functionality
+
+#returns single article that user hasn't solved (if solved all, returns a random article) 
+#as string as tuple of (title, content, link), it also marks the returned article as read
+def getArticleToSolve(user_id):
+    if user_id is None:
+        raise ExecutionAbort("[DATA] Cannot get article for user that is not signed in.")
+     
+    try:
+        solvedArticleIDs = __getArticlesSolved__(user_id)
+
+        allArticleIDs = __getAllArticles__()
+
+        availableArticles = [article for article in allArticleIDs if article not in solvedArticleIDs]
+        
+        articleToSolve = random.choice(allArticleIDs) if not availableArticles else random.choice(availableArticles) 
+     
+        __addArticleAsSolved__(articleToSolve)
+
+        return __getArticleByID__(articleToSolve)
+
+    except:
+        raise ExecutionAbort("[DATA] Error while fetching user's available article data.")
+
+#METHODS BELOW ARE NOT FOR FRONTEND CALLS
+def __getAllArticles__():
+    articlesSnapshot = db.collection("articles").get()
+
+    return [a.id for a in articlesSnapshot]
+
+def __getArticleByID__(article_id):
+    if article_id is None:
+        raise ExecutionAbort("[DATA] Cannot get article for non-existent article id.")
+
+    articleSnapshot = db.collection("articles").document(article_id).get().to_dict()
+
+    articleTitle = articleSnapshot.get("title")
+    articleContent = articleSnapshot.get("content")
+    articleLink = articleSnapshot.get("link")
+
+    return articleTitle, articleContent, articleLink
+
+def __getArticlesSolved__(user_id):
+    if user_id is None:
+        raise ExecutionAbort("[DATA] Cannot get article data for user that is not signed in.")
+    
+    try:
+        userSnapshot = db.collection("users").document(user_id).get().to_dict()
+        print("[DATA] Successfully fetched user's read articles.")
+
+        return userSnapshot.get("readArticles", [])
+    except:
+        raise ExecutionAbort("[DATA] Error while fetching user's article data.")
+    
+def __addArticleAsSolved__(user_id, article_id):
+    if user_id is None:
+        raise ExecutionAbort("[DATA] Cannot get article data for user that is not signed in.")
+    
+    try:
+        db.collection("users").document(user_id).update({"readArticles": (__getArticlesSolved__(user_id) + [article_id])})
+        print("[DATA] Successfully updated user's read articles.")
+
+    except:
+        raise ExecutionAbort("[DATA] Error while fetching user's article data.")
