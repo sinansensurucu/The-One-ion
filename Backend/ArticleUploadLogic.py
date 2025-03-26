b3972aa6a13097aa66c43181ea72d09aab96ea35
sinansensurucuwewalk <sinan@wewalk.io>
2025-03-26 13:57:05 +0000

    changed file types

--- Backend/ArticleUploadLogic.py
+++ Backend/ArticleUploadLogic.py
@@ -3,14 +3,12 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 
-# Initialize Firebase
 current_dir = os.path.dirname(os.path.abspath(__file__))
 cred = credentials.Certificate(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Credentials', 'firebaseCredentials.json'))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def upload_articles():
-
     data_dir = os.path.join(current_dir, 'Articles')
     
     article_types = {
@@ -24,18 +22,14 @@ def upload_articles():
             articles = json.load(f)
         
         for article_id, article_data in articles.items():
-            # creates the id so its like real-1 and stuff
             doc_id = f"{article_type}_{article_id.split('_')[-1]}"
             
-            # this column is for answer 
-            article_data['answer'] = article_type.capitalize()  # "Real", "Fake" or "Onion"
+            article_data['answer'] = article_type.capitalize()
             
-            # upload to database
             db.collection('articles').document(doc_id).set(article_data)
-            print(f"Uploaded {doc_id}")
+            print(f"[UPLOAD] Uploaded article with id: {doc_id}.")
 
 def upload_statistics():
-
     data_dir = os.path.join(current_dir, 'Statistics')
     
     statistic_type = {
@@ -48,17 +42,16 @@ def upload_statistics():
             articles = json.load(f)
         
         for statistic_id, statistic_data in articles.items():
-            # creates the id so its like real-1 and stuff
             doc_id = f"{statistic_type}_{statistic_id.split('_')[-1]}"
             
-            # this column is for answer 
             statistic_data['answer'] = statistic_type.capitalize()
             
-            # upload to database
             db.collection('statistics').document(doc_id).set(statistic_data)
-            print(f"Uploaded {doc_id}")
+            print(f"[UPLOAD] Uploaded statistic with id: {doc_id}.")
 
 if __name__ == "__main__":
     upload_articles()
+    print("[UPLOAD] Articles done uploading.")
     upload_statistics()
-    print("done")
+    print("[UPLOAD] Statistics done uploading.")
+    
